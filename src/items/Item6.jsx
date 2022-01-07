8efeedf5528997a5397d1324475ce8993ea2fb0c
Abdul Latif Mubasir <bj.angel119@gmail.com>
2022-01-07 13:44:41 +0700

    create upload file

--- src/items/Item6.jsx
+++ src/items/Item6.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { FilePond, File, registerPlugin } from 'react-filepond'
+import 'filepond/dist/filepond.min.css'
 import { Stack  ,Typography , Box , Button } from '@mui/material'
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 const Item6 = ({pageDown}) => {
   return (
@@ -8,6 +15,13 @@ const Item6 = ({pageDown}) => {
       <Typography variant="h5" sx={{marginBottom: 1}}>Please upload the social media screenshots where you mention Myriad.Social to this page!</Typography>
       <Box>
         <Typography variant='body'>If you have multiple files, you can upload a zipped folder.</Typography>
+      </Box>
+      <Box sx={{marginTop: 2}}>
+        <FilePond
+          allowMultiple={false}
+          name="files" 
+          labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        />
       </Box>
       <Stack  direction={'row'} alignItems={'center'} spacing={2}>
         <Button sx={{marginTop: 2}} onClick={pageDown} variant="contained" endIcon={<CheckRoundedIcon />} >OK</Button>
